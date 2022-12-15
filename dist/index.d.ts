24c05e1cbcf36ef9308172b3a8b2d9ab8ab5caea
Jaron Wanderley <jaronwanderley@gmail.com>
2022-12-15 20:58:37 -0300

    Update index.d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 export declare const range: (start?: number, end?: number, step?: number) => number[];
-e: string) => HTMLElement;
+export declare const typeOf: (element: any) => any;
+export declare const createEl: (type: string) => HTMLElement;
 export declare const setClass: (element: HTMLElement, className: string) => void[];
 export declare const removeClass: (element: HTMLElement, className: string) => void[];
 export declare const setStyle: (element: HTMLElement, object: Object) => any[];
@@ -10,3 +11,15 @@ export declare const platform: () => {
     isDark: boolean;
     isLight: boolean;
 };
+export interface Point {
+    x: number;
+    y: number;
+    [x: string | number | symbol]: any;
+}
+export declare const getDistance: (p1: Point, p2: Point) => number;
+export declare const loadText: (path: string) => Promise<string>;
+export declare const loadJson: (path: string) => Promise<any>;
+export declare const clamp: (value: number, min: number, max: number) => number;
+export declare const get: (path: string, obj?: Window | Object) => any;
+export declare const set: (path: string, object: Object | Window | undefined, value: any) => any;
+export declare const getListOfPaths: (obj?: object) => any;
