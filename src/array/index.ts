9b6e26a5fb368bebc138e7fb65d17448b80e599e
Jaron Wanderley <jaronwanderley@gmail.com>
2023-02-21 12:53:25 -0300

    update range function

--- src/array/index.ts
+++ src/array/index.ts
@@ -1,3 +1,9 @@
-export const range = (start = 0, end = 0, step = 1) => Array(Math.floor((end - start) / step + 1))
-  .fill(0)
-  .map((_, index) => step * index + start)
+export const range = (start = 0, end = 0, step = 1) => {
+  const { floor, abs } = Math
+  if (step === 0)
+    return []
+  step = (end < start ? -1 : 1) * abs(step)
+  return Array(floor((end - start) / step) + 1)
+    .fill(0)
+    .map((_, index) => step * index + start)
+}
